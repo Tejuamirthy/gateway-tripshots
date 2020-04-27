23e67cd50b24e8dba8c10f67d46de692f87fb8b6
tejuamirthi <tejuamirthi@gmail.com>
2020-04-27 12:48:29 +0530

    config login logout handlers commit

---
+++ src/main/java/com/tripshots/gateway/config/LogoutHandler.java
@@ -0,0 +1,29 @@
+package com.tripshots.gateway.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+public class LogoutHandler implements LogoutSuccessHandler {
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request,
+                                HttpServletResponse response, Authentication authentication)
+            throws IOException, ServletException {
+        if(authentication != null) {
+            System.out.println(authentication.getName());
+        }
+        //perform other required operation
+        response.setStatus(HttpStatus.OK.value());
+        PrintWriter pw = response.getWriter();
+        pw.write("{'response': 'successful'}");
+        pw.flush();
+    }
+}
